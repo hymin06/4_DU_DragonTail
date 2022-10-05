affa2732fbc9b869b3a8c5fe42ede95729edcafc
Choimin0301 <98930093+Choimin0301@users.noreply.github.com>
2022-10-05 20:44:18 +0900

    asdf

--- 4_DU_YONGMI PJ/Assets/01_Script/_MinHyeok/UpDown.cs
+++ 4_DU_YONGMI PJ/Assets/01_Script/_MinHyeok/UpDown.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class UpDown : MonoBehaviour
 {
+<<<<<<< Updated upstream
     public GameObject SulChang;
     public GameObject KeySulChang;
 
@@ -27,6 +28,30 @@ public class UpDown : MonoBehaviour
     void Start()
     {
         
+=======
+    public GameObject SettingPan;
+    public GameObject OtherPan;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //SettingPan.SetActive(false);
+    }
+    public void OtherPanUp()
+    {
+        OtherPan.SetActive(true);
+    }
+    public void OtherPanDown()
+    {
+        OtherPan.SetActive(false);
+    }
+    public void SettingPanUp() 
+    {
+        SettingPan.SetActive(true);
+    }
+    public void SettingPanDown()
+    {
+        SettingPan.SetActive(false );
+>>>>>>> Stashed changes
     }
 
     // Update is called once per frame
