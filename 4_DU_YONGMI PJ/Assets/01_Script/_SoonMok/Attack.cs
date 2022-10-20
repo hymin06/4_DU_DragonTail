60a8fa0b8bdc300244163c3051d5b8c914a80975
ban324 <ban324@naver.com>
2022-10-20 19:00:50 +0900

    BiteTheDust

---
+++ 4_DU_YONGMI PJ/Assets/01_Script/_SoonMok/Attack.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    [SerializeField]GameObject _weapon;
+    [SerializeField]GameObject _grabPoint;
+    [SerializeField]Weapon _weaponsc;
+    public float Power;
+    [SerializeField] float angle;
+    [SerializeField] Vector2 dir;
+
+    void Update()
+    {
+        if (_weapon)
+        {
+            _weapon.transform.position = _grabPoint.transform.position;
+            dir = Camera.main.ScreenToWorldPoint(Input.mousePosition) - _grabPoint.transform.position;
+            angle = Mathf.Atan2(dir.normalized.y, dir.normalized.x) * Mathf.Rad2Deg;
+            _weapon.transform.rotation = Quaternion.Euler(0, 0, angle);
+            if (Input.GetMouseButtonDown(0))
+            {
+                _weaponsc.Shooting(dir, Power);
+                _weaponsc = null;
+                _weapon = null;
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.GetComponent<Weapon>() && !collision.gameObject.GetComponent<Weapon>().IsShooting)
+        {
+            _weapon = collision.gameObject;
+            _weaponsc = collision.gameObject.GetComponent<Weapon>();
+            _weaponsc.Grab(); 
+        }
+
+    }
+}
